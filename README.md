d58f0fa0dae04c18db6114e213b19a85dce33924
Jonas Tingeborn <tinjon@gmail.com>
2015-11-14 18:51:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Page Collect
+Collects paginated data, data spread over many linked pages.
+
+Abstract
+--------
+A function mainly intended to simplify data extraction and page navigation tasks for web automation, such as user scripts for browsers, where often all data of interest is not presented on a single page, but spread out (paginate) over a collection of pages.
+
+The function processes the current page, scans it for a link to a following page and repeats the process until the entire paginated set has been processed. Once the last page has been encountered, the data collected from each page is provided as an array to a handler function.
+
+The elements of the array handed to the handler function correspond to the data items extracted during the page traversal.
+
+Pagination collection function
+------------------------------
+**Signature**: `pageCollect(nextLinkSelector, contentSelector)`  
+**Returns**: A jQuery promise ("deferred") object.  
+**Arguments**:
+
+* `nextLinkSelector`: Extractor
+  This extractor is expected to return a URL (a string) signifying the next page to process. When the function returns a "falsy" value (false / null / undefined), it is treated as a signal that all pages have been processed,    causing the deferred handler will get invoked.
+
+* `contentSelector`: Extractor
+  Function responsible for extracting data from the pages. If data is returned, it will be appended to the result array handed over to the promise `done` handler when there are no more pages to process.
+
+_For more information on the Extractor type, see below_
+
+Examples
+--------
+Extract some data from all paginated pages using CSS selector expressions.
+````javascript
+pageCollect(".nextlink | href", ".some-content | text")
+
+.done(function(items) {
+  console.log("extracted items:", items);
+})
+.fail(function(err) {
+  console.error("Uh Oh, something went wrong", err);
+});
+````
+
+The same extraction behavior using custom data extraction functions.
+
+````javascript
+pageCollect(function(doc) {
+  return $(doc).find(".nextlink").attr('href');
+}, function(doc) {
+  return $(doc).find(".some-content").text();
+})
+
+.done(...) .fail(...)
+````
+
+Extractor
+---------
+Data extraction is performed using the notion of an "extractor". An extractor takes the shape of one of two forms; A slightly extended CSS expression or a user provided function. The definition for an extractor is:
+
+    CSS expression | function(element, context)
+
+Extractor as a CSS selector
+---------------------------
+When in the form of a CSS expression, the expression is applied to the entire document, just like issuing `$(expr, document)`. The _extended_ bit refers to a syntax extension used to communicate *what* to extract from matched
+elements.
+
+To declare what attribute to extract from matched elements, the CSS expression is terminated with `|<attribute name>`. If no extraction attribute sub-expression is provided, the extractor will simply return the extracted elements, to be further processed in the user's deferred handler once all pages have been processed.
+
+Examples of expressions and the associated effects:
+
+    a      -> An array of all links on the pages in the paginated set. Same as performing $('a') on a page.
+    a|text -> All contained link texts, same as issuing $(el).text() on links.
+    a|html -> All contained HTML in the links, same as issuing $(el).html() on each link.
+    a|href -> The URLs (strings) of all links in the page set, same as issuing $(el).attr('href') on the links.
+
+Note: The attribute names _text_ and _html_ are the only ones with special meaning. Any other attribute name will result in the equivalent DOM element attribute being used for extraction. I.e. `"a|foo"` is equivalent to `$(el).attr('foo')` being performed on all elements matching the CSS filter expression (the part before the pipe character, `"a"` in this case).
+
+Extractor as a function
+-----------------------
+For more sophisticated data extraction needs, a user provided function can also be used as an extractor.
+
+**Signature**: `function(element, context)`  
+**Arguments**:
+
+* `element`
+    The function will be handed a DOM element containing the page elements. Regard this element as the page root node (e.g. document or something similar during background / ajax fetching and collation).
+
+* `context`
+    Also provided is a function global object called 'context'. The purpose of this object is to allow sharing of state between the extractor invocations. Each time an extractor is invoked, it will be handed the same object, making it a convenient place to temporarily store information during the data extraction.
+
+    Also worth noting is that each time a new page gets processed, the context object's `url` attribute gets set to the URL of the page being processed, which might be handy information to have in certain circumstances.
---
+++ page-collect.js
@@ -0,0 +1,154 @@
+/**
+ * pageCollect - Navigates and extracts data from a set of linked pages
+ *
+ * Copyright (C) 2015 Jonas Tingeborn
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * Refer to the README for specification and usage examples.
+ * Requires jQuery 1.5+
+ */
+function pageCollect(nextLinkSelector, contentSelector) {
+  var $ = typeof(jQuery) == "function" ? jQuery : typeof($) == "function" ? $ : undefined;
+      results = [],
+      context = {url: window.location.href},
+      deferred;
+
+  (function assertPreReqsSatisfied() {
+    if(!($ && $.fn && $.fn.jquery)) throw "jQuery not loaded";
+    if($.fn.jquery < "1.5") throw "jQuery 1.5+ required, version "+ $.fn.jquery +" is loaded";
+  })();
+
+  function debug() {
+    if(typeof(console) != 'undefined' && console.debug) {
+      console.debug.apply(console, Array.prototype.slice.call(arguments));
+    }
+  }
+
+  function error() {
+    if(typeof(console) != 'undefined' && console.error) {
+      console.error.apply(console, Array.prototype.slice.call(arguments));
+    }
+    deferred.reject(msg);
+  }
+
+  // Ordinary jquery get, except for stripping the X-Requested-With header,
+  // since some sites behave strangely when that header is set.
+  function get(url) {
+    debug("fetching page: "+ url);
+    return $.ajax({
+      method: 'GET',
+      url: url,
+      xhr: function() {
+          var xhr = jQuery.ajaxSettings.xhr();
+          var setRequestHeader = xhr.setRequestHeader;
+          xhr.setRequestHeader = function(name, value) {
+              if (name == 'X-Requested-With') return;
+              setRequestHeader.call(this, name, value);
+          }
+          return xhr;
+      }
+    });
+  }
+
+  // Convert the raw HTML from an Ajax-request to a DOM object the user
+  // can more easily operate on.
+  function processAjaxPage(html) {
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    process(div);
+    div.innerHTML = '';
+  }
+
+  // Extract content and next-link using the user-defined methods for doing
+  // so, and repeat the procedure for each page as long a URL is found pointing
+  // to a linked "next-page".
+  function process(doc) {
+    var url, data;
+
+    try {
+      data = contentSelector(doc, context)
+      results.push(data);
+    } catch(err){
+      error(err);
+    }
+
+    try {
+      url = nextLinkSelector(doc, context);
+      if(typeof(url) != "string") url = url[0];
+    } catch(err){
+      error(err);
+    }
+
+    if(url) {
+      context.url = url;
+      get(url).success(processAjaxPage).fail(function(xhr, kind, msg){
+        error({type: kind, reason: msg});
+      });
+    } else {
+      results = [].concat.apply([], results);  // flatten array
+      deferred.resolve(results);
+    }
+  }
+
+  // Convert css selectors to function to simplify branching logic later on.
+  // E.g. ".foo|bar" where '.foo' becomes the filter and 'bar' the attribute
+  // to extract on matched elements.
+  function normalizeSelector(name, selector) {
+    function createSelectorFunction() {
+      var filter, attr, m;
+
+      if(m = selector.match(/^(.+?)(\s*\|\s*[^|\]]+)?$/)) {
+        filter = m[1];
+        if(m[2]) {
+          attr = m[2].replace('|','').trim();
+        }
+      } else {
+        throw name + " is an invalid CSS selector extraction expression";
+      }
+
+      if(attr) {
+        return function(doc) {
+          return $(filter, doc).map(function() {
+            if(attr == 'text') {
+              return $(this).text();
+            } else if(attr == 'html') {
+              return $(this).html();
+            } else {
+              return $(this).attr(attr);
+            }
+          }).get();
+        };
+      } else {
+        return function(doc) {
+          return $(filter, doc);
+        };
+      }
+    }
+
+    if(typeof(selector) == 'function') return selector;
+    if(typeof(selector) == 'string' && selector.length > 0) {
+      return createSelectorFunction();
+    }
+    throw name + " is neither a function nor a css selector string";
+  }
+
+  deferred = $.Deferred();
+  nextLinkSelector = normalizeSelector('nextLinkSelector', nextLinkSelector);
+  contentSelector  = normalizeSelector('contentSelector', contentSelector);
+  process(window.document);
+  return deferred.promise();
+}
